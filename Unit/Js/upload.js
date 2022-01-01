308f8cbdfc1cd956ef1aee72393e5655cf8f28be
Ishaan Kalra <74290686+IshaanKalra2103@users.noreply.github.com>
2022-01-01 22:29:58 +0530

    First Commit
    This is the first commit for this project.

---
+++ Unit/Js/upload.js
@@ -0,0 +1,12 @@
+var ImgName , ImgUrl;
+var files = [];
+var reader = new FileReader();
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBx55_Pcj9HNDp33FIlxWMzrW-CPDu04CY",
+    authDomain: "insight-a49e5.firebaseapp.com",
+    projectId: "insight-a49e5",
+    storageBucket: "insight-a49e5.appspot.com",
+    messagingSenderId: "948990251053",
+    appId: "1:948990251053:web:6f7f00982c62ce307c68a4"
+  };
