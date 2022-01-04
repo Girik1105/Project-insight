8cbc1db1197ad2bdc7bb792daf68e290724d99e6
Ishaan Kalra <74290686+IshaanKalra2103@users.noreply.github.com>
2022-01-04 21:37:16 +0530

    Retail video implemented

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
 
     path('department-store/video/', views.department_store_video, name="department_store_video"),
     path('wholesale/video/', views.wholesale_video, name="wholesale_video"),
+    path('retail/video/', views.retail_video, name="retail_video"),
 
     path('quiz/', views.quiz_index, name="quiz_index"),
     path('quiz/game/', views.quiz_game, name="quiz_game"),
