59480ac34554dc60d1f7d92e6818e7494f51c065
Girik1105 <girik1105@gmail.com>
2022-02-25 04:04:44 +0530

    Added backend for forums, score + continued tweakinf frontend

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
 # Create your models here.
+class user_score(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
