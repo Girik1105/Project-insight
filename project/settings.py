0113808d70fae450534e350038d487ec1032f536
Girik1105 <girik1105@gmail.com>
2022-02-28 12:32:05 +0530

    db added

--- project/settings.py
+++ project/settings.py
@@ -124,9 +124,9 @@ if DEBUG:
         }
     }
 
-if not DEBUG:
-    db_from_env = dj_database_url.config(conn_max_age=600)
-    DATABASES['default'].update(db_from_env)
+
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
