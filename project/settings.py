854f34ec6adb92704c200805ce48137700476b9a
Girik1105 <girik1105@gmail.com>
2022-02-28 10:54:51 +0530

    Added s3 config

--- project/settings.py
+++ project/settings.py
@@ -64,6 +64,8 @@ INSTALLED_APPS = [
     "forums",
 
     'bootstrap4',
+
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -174,6 +176,18 @@ else:
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+if not DEBUG:
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
+AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID') 
+AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
